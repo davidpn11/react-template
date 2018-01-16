0b8cf120a098c55c9b387394e27c05953a3a797e
davidpn11 <davidn@take.net>
2018-01-16 10:24:21 -0200

    added prettier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
+    const x = 21
     return (
       <div className="App">
         <header className="App-header">
@@ -14,8 +15,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
